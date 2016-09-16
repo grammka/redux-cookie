c245f67e23c0412faf63fb71da28b2ec68f42963
grammka <grammka@gmail.com>
2016-09-16 21:31:48 +0300

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # redux-cookie-persist
-Persist a redux store with cookies
+Redux cookie persister and middleware
+
+[![Npm Version](https://badge.fury.io/js/redux-cookie-persist.svg)](https://www.npmjs.com/package/redux-cookie-persist)
+[![Month Downloads](https://img.shields.io/npm/dm/redux-cookie-persist.svg)](http://npm-stat.com/charts.html?package=redux-cookie-persist)
+[![Npm Licence](https://img.shields.io/npm/l/redux-cookie-persist.svg)](https://www.npmjs.com/package/redux-cookie-persist)
+
+[![NPM](https://nodei.co/npm/redux-cookie-persist.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/redux-cookie-persist/)
+
+
+## Install
+
+```bash
+npm install --save redux-cookie-persist
+```
+
+
+## Usage
+
+```javascript
+middleware: [
+  reduxCookieMiddleware({
+    'ACTION_TYPE': {
+      reducerKey: 'STATE_SUBSET_PATH',
+      cookieKey: 'COOKIE_KEY'
+    },
+  })
+],
+
+enhancers: [
+  persistState({
+    [COOKIE_KEY]: 'STATE_SUBSET_PATH',
+  })
+],
+```
+
+
+## Example
+
+```javascript
+import { persistState, reduxCookieMiddleware } from 'redux-cookie-persist'
+
+reduxCookieMiddleware({
+  'session.setToken': {
+    reducerKey: 'session.token',
+    cookieKey: 'token'
+  },
+})
+
+persistState({
+  token: 'session.token',
+})
+```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import cookie from 'react-cookie'
+import Immutable from 'immutable'
 
 
-const persistState = (params) => (store) => (next) => (action) => {
+const persistState = (paths) => (next) => (reducer, initialState, enhancer) => {
+  if (typeof initialState === 'function' && typeof enhancer === 'undefined') {
+    enhancer = initialState
+    initialState = undefined
+  }
+
+  let finalInitialState
+
+  try {
+    let persistedState = Immutable.Map({})
+  
+    for (const cookieKey in paths) {
+      const storePath   = paths[cookieKey].split('.')
+      const value       = cookie.load(cookieKey)
+  
+      if (typeof value != 'undefined') {
+        persistedState = persistedState.setIn(storePath, value)
+      }
+    }
+  
+    finalInitialState = initialState.mergeDeep(initialState, persistedState)
+  } 
+  catch (e) {
+    console.warn('Failed to retrieve initialize state from Cookie Storage:', e)
+  }
+
+  return next(reducer, finalInitialState, enhancer)
+}
+
+const reduxCookieMiddleware = (params) => (store) => (next) => (action) => {
   const result = next(action)
 
   if (action.type in params) {
-    const key     = params[action.type].split('.')
-    const value   = store.getState().getIn(key)
+    const keys    = params[action.type]
+    const value   = store.getState().getIn(keys.reducerKey.split('.'))
     const year    = 365 * 24 * 60 * 60
 
-    cookie.save(key, value, {
+    cookie.save(keys.cookieKey, value, {
       hostOnly: true,
       maxAge: year
     })
@@ -18,4 +48,8 @@ const persistState = (params) => (store) => (next) => (action) => {
   return result
 }
 
-export default persistState
+
+export default {
+  persistState,
+  reduxCookieMiddleware
+}
